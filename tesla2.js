d658cc1ec9fb2e7ca346fd22ce69fff69e5ef546
fluf662 <yyhh6650842@gmail.com>
2026-03-19 17:54:21 +0500

    Add files via upload

--- tesla2.js
+++ tesla2.js
@@ -1,37 +1,39 @@
+// таймер
 (function () {
   const second = 1000,
         minute = second * 60,
         hour = minute * 60,
         day = hour * 24;
 
-  let today = new Date(),
-      yyyy = today.getFullYear(),
-      date = "05/15/" + yyyy;
+  const countDown = new Date("May 15, 2026 00:00:00").getTime();
 
-  const countDown = new Date(date).getTime(),
-        x = setInterval(function () {
+  setInterval(function () {
 
-          const now = new Date().getTime(),
-                distance = countDown - now;
+    const now = new Date().getTime(),
+          distance = countDown - now;
 
-          document.getElementById("days").innerText = Math.floor(distance / day);
-          document.getElementById("hours").innerText = Math.floor((distance % day) / hour);
-          document.getElementById("minutes").innerText = Math.floor((distance % hour) / minute);
-          document.getElementById("seconds").innerText = Math.floor((distance % minute) / second);
+    document.getElementById("days").innerText = Math.floor(distance / day);
+    document.getElementById("hours").innerText = Math.floor((distance % day) / hour);
+    document.getElementById("minutes").innerText = Math.floor((distance % hour) / minute);
+    document.getElementById("seconds").innerText = Math.floor((distance % minute) / second);
 
-        }, 1000);
+  }, 1000);
 })();
 
-// СПОЙЛЕР
+// кнопка
 var div = document.getElementById('spoiler_text');
 var btn = document.getElementById('spoiler_button');
 
 btn.onclick = function() {
-  if (div.className === 'fadeout') {
-    div.className = 'fadein';
-    btn.innerHTML = 'скрыть';
+
+  if (div.classList.contains('fadeout')) {
+    div.classList.remove('fadeout');
+    div.classList.add('fadein');
+    btn.innerHTML = 'Скрыть условия';
   } else {
-    div.className = 'fadeout';
-    btn.innerHTML = 'узнать';
+    div.classList.remove('fadein');
+    div.classList.add('fadeout');
+    btn.innerHTML = 'Узнать условия';
   }
+
 };
